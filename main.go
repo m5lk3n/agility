ed2268974b9923352bfd77d16de8a7728d0e1797
Michael Klein <dev@michael-klein.info>
2021-01-09 20:39:24 +0100

    watching deployments

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"path/filepath"
 
+	v1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -33,11 +36,32 @@ func main() {
 	}
 
 	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceAll)
+
+	// list deployments
+	fmt.Println("listing deployments:")
 	list, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
 	for _, d := range list.Items {
 		fmt.Printf(" * %s\n", d.Name)
+	}
+
+	// watch deployments
+	fmt.Println("watching deployments:")
+	watcher, err := deploymentsClient.Watch(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	ch := watcher.ResultChan()
+	for event := range ch {
+		deployment, ok := event.Object.(*v1.Deployment)
+		if !ok {
+			log.Fatalln("unexpected type")
+		}
+		switch event.Type {
+		case watch.Added:
+			fmt.Printf(" * %s deployed\n", deployment.Name)
+		}
 	}
 }
